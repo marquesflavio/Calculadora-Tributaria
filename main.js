77b49cc93cc588d97834c5c55f896078bc34d894
Flávio Marques <flaviomarques2@hotmail.com>
2023-04-27 20:26:18 -0300

    finalizei calculos anexo v

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const btn = document.querySelector("#calcular")
 
-//Função para pegar o valor do input Receita Mensal e calcular o valor do imposto (15,5%)
+//Função para pegar o valor do input Receita Mensal e calcular o valor do imposto (15,5%) e o INSS (11%)
+
 btn.addEventListener("click", function(e){
     e.preventDefault();
 
     const receitaMensal = document.querySelector('#receita_mensalV')
     const recMes = receitaMensal.value;
-    
-    // console.log(recMes)
+
+    const proLabore = document.querySelector('#pro-laboreV')
+    const proLab = proLabore.value;
 
     let imposto = document.querySelector('#impostoV');
     let calcImposto = parseInt(recMes * 0.155);
 
+    let inssV = document.querySelector('#inssV');
+    let calcInss = parseInt(proLab * 0.11);
+
     imposto.value = calcImposto
-    // console.log(calcImposto)
+
+    inssV.value = calcInss  
 
 })
