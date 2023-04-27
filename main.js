4ed118c971fbae54b33e5cdb52261784f0108428
Flávio Marques <flaviomarques2@hotmail.com>
2023-04-27 09:12:15 -0300

    Crei lógica calculadora JS

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const btn = document.querySelector("#calcular")
 
+//Função para pegar o valor do input Receita Mensal e calcular o valor do imposto (15,5%)
 btn.addEventListener("click", function(e){
     e.preventDefault();
 
     const receitaMensal = document.querySelector('#receita_mensalV')
     const recMes = receitaMensal.value;
     
-    console.log(recMes)
+    // console.log(recMes)
 
     let imposto = document.querySelector('#impostoV');
-    
-    let Calcimposto = parseInt(recMes * 0.155);
+    let calcImposto = parseInt(recMes * 0.155);
 
-    imposto.innerHTML(`${Calcimposto}`)
-
+    imposto.value = calcImposto
+    // console.log(calcImposto)
 
 })
