7cc3f6ba429b816057f25c2a2c760e29b1358f9f
Flávio Marques <flaviomarques2@hotmail.com>
2023-04-27 21:25:24 -0300

    finalizado sessão principal

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Primeira missão diária
 Essa single page aplication conta com uma cálculadora de avaliação tributária para empresas do simples nacional que desempenham atividades do anexo v, ou seja, nós DEVs!
 
 [X] Criar toda estrutura HTML
-[] Estilizar com CSS
-[] Fazer a calculadora e as funções com JS
+[X] Estilizar com CSS
+[X] Fazer a calculadora e as funções com JS
 [] Criar algumas aplicações interessantes com CSS.
+[] Criar um botão para limpar campos de input e de respostas
 [] Formatar campos para valores monetários
 
--- main.js
+++ main.js
@@ -21,4 +21,41 @@ btn.addEventListener("click", function(e){
 
     inssV.value = calcInss  
 
+    //Calculadora do anexo V com redução
+
+    const receitaMensalReducao = document.querySelector('#receita_mensal')
+    const recMesRed = receitaMensalReducao.value;
+
+    let impostoReducao = document.querySelector('#imposto')
+    let calcImpostoRed = parseInt(recMesRed * 0.06);
+
+    impostoReducao.value = calcImpostoRed;
+
+    let proLaboreReducao = document.querySelector('#pro_labore')
+    let calcProLaboreRed = parseFloat(recMesRed * 0.28);
+
+    proLaboreReducao.value = calcProLaboreRed;
+
+    let inssReducao = document.querySelector('#inss')
+    let calcInssRed = parseInt(calcProLaboreRed * 0.11);
+
+    inssReducao.value = calcInssRed
+
+    let somaImpostos = calcImposto + calcInss ;
+    let somaImpostosRed = calcImpostoRed + calcInssRed ;
+    let economiaTribut = somaImpostos - somaImpostosRed
+
+    //Mostra o resultado no HTML
+    const para = document.createElement("p");
+    para.innerHTML = `Aplicando a legislação, o total de tributos federais a pagar são R$ ${somaImpostos} de Simples Nacional e INSS, já com a aplicação da redução o total ficou R$ ${somaImpostosRed}, representando, portanto, uma economia de R$ ${economiaTribut}`;
+    
+    document.getElementById('resultado').appendChild(para);
+    
 })
+
+function zerarCampos(){
+    document.getElementsByTagName('input').value = ''
+
+   
+
+}
